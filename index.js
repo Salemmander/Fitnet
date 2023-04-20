e569fec4da2a78aa6cfeafa51df0c2ee110fab6c
Mousa Najjar <113804025+mousanajjar213@users.noreply.github.com>
2023-04-20 01:06:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const placeholderLinks = document.querySelectorAll('.nav-link');
 placeholderLinks.forEach(link => {
   link.addEventListener('click', (event) => {
     event.preventDefault();
-    window.open('https://example.com', '_blank', 'width=500,height=500');
+    window.open('https://github.com/Salemmander/Fitnet', '_blank', 'width=500,height=500');
   });
 });
