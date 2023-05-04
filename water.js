80a1670bcb6a479bdfde2c11d569fd3bec3bd8ac
Salemmander <salemnassar5@gmail.com>
2023-05-04 16:38:25 -0500

    implemented water tracker :)

--- water.js
+++ water.js
@@ -1,1 +1,22 @@
-alert("CHALLENGE!! Do 20 pushups");
+let totalWater = 0;
+const goal = 2000;
+
+function addWater() {
+    const waterInput = document.getElementById("water-input");
+    const progressBar = document.getElementById("progress-bar");
+    const waterInfo = document.getElementById("water-info");
+
+    let waterAmount = parseInt(waterInput.value);
+    if (!isNaN(waterAmount) && waterAmount > 0) {
+        totalWater += waterAmount;
+        if (totalWater > goal) {
+            waterInfo.textContent = "You went over your goal!";
+        } else {
+            let progress = totalWater;
+            progressBar.style.width = progress + "%";
+            waterInfo.textContent = totalWater + "/" + goal + "ml";
+        }
+    }
+
+    waterInput.value = "";
+}
